c5f2f53c83eb3625f1a8409337cb2afb46741de9
jun.oyanagi <jun.oyanagi@persol.co.jp>
2020-10-07 13:45:08 +0900

    WIP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-	res.write('<h1>Hello World</h1>');
+	res.write('<h1>Hello World!!</h1>');
 	res.end();
 });
 
